ff8696a9b83195082444344fb728e98599ed4b58
Eugen Fischer <eugen.fischer.android@googlemail.com>
2017-09-22 20:26:03 +0200

    BMP180 Sensor hinzugefügt, JSON erweitert

--- src/AWS.ino
+++ src/AWS.ino
@@ -14,140 +14,200 @@
 // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
 
 #include <Arduino.h>
+#include <Wire.h>
 #include "dht.h"
 #include <stdlib.h>
 #include <SoftwareSerial.h>
+#include "SFE_BMP180.h"
 
 #define DHTPIN 2     // Digitalpin an dem wir angeschlossen sind.
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 #define SerialMethod 1 //0 = USB, 1 = Bluetooth
 #define Fehlermeldung "Fehler beim Lesen des DHT Sensors!"
+#define Hoehe 450.0 // Höhe in Aalen in Metern
 
 DHT dht(DHTPIN, DHTTYPE);
 //Tx = 1, Rx = 0
 SoftwareSerial BT(1, 0);
+SFE_BMP180 BMP180;
 
 void setup() {
   if (SerialMethod == 0){
+    //Initialisierung der Serialverbindung
     Serial.begin(9600);
     Serial.println("AWS - Arduino Wetter Server");
+
+    //Initialisieren den BMP180 Sensor
+    if (BMP180.begin()){
+      Serial.println("BMP180 Initialisierung erfolgreich");
+    }
+    else{
+      Serial.println("BMP180 Initialisierung nicht erfolgreich\n\n");
+    }
   }
   if (SerialMethod == 1){
+    //Initialisierung der Bluetoothverbindung
     BT.begin(9600);
     Serial.println("AWS - Arduino Wetter Server");
+    //Initialisieren den BMP180 Sensor
+    if (BMP180.begin()){
+      Serial.println("BMP180 Initialisierung erfolgreich");
+    }
+    else{
+      Serial.println("BMP180 Initialisierung nicht erfolgreich\n\n");
+    }
   }
   dht.begin();
 }
 
-String json_generator(const double &feuchte, const double &temperatur, const int &widerstand){
+String json_generator(const double &feuchte, const double &temp_am2302, const double &temp_bmp180, const int &druckwert, const int &widerstand){
+
   char json_feuchte[5];
   dtostrf(feuchte,3,1,json_feuchte);
-  
-  char json_temperatur[5];
-  dtostrf(temperatur,3,1,json_temperatur);
 
-  return "{\"Luftfeuchte\": " + String(json_feuchte) + ", " + "\"Temperatur\": " + String(json_temperatur) + ", " + "\"Photostrom\": " +  String(widerstand) + "}";
+  char json_temperatur_am2302[5];
+  dtostrf(temp_am2302,3,1,json_temperatur_am2302);
+  
+  char json_temperatur_bmp180[5];
+  dtostrf(temp_bmp180,3,1,json_temperatur_bmp180);
+
+  return "{\"Luftfeuchte\": " + String(json_feuchte) + ", " + "\"Temp_am2302\": " + String(json_temperatur_am2302)+ ", " + "\"Temp_bmp180\": " + String(json_temperatur_bmp180) + ", " + "\"Luftdruck\": " +  String(druckwert) + ", " + "\"Photostrom\": " + String(widerstand) +"}";
 }
 
 String seriell_auslesen(int modus){
   String serial_nachricht = "";
   char serial_char;
-  
+
   //Serielle USB Verbindung
   if (modus == 0){
     while (Serial.available()){
-    serial_char = Serial.read();
-    serial_nachricht.concat(serial_char);
+      serial_char = Serial.read();
+      serial_nachricht.concat(serial_char);
     }
   }
-  
+
   //Serielle BT Verbindung
   if (modus == 1){
     while (BT.available()){
-    serial_char = BT.read();
-    serial_nachricht.concat(serial_char);
+      serial_char = BT.read();
+      serial_nachricht.concat(serial_char);
     }
   }
-  
+
   return serial_nachricht;
 }
 
 boolean seriell_senden(int modus, const String &serial_anfrage, const String &json_string){
-  
+
   boolean stat;
-  
+
   if (serial_anfrage != ""){
     if (serial_anfrage == "sensor_anfrage"){
       if (modus == 0){
-          Serial.println(json_string);
-        }
+        Serial.println(json_string);
+      }
       if (modus == 1){
-          BT.println(json_string);
-        }
+        BT.println(json_string);
+      }
     }
-   else{
-     if (modus == 0){
-       Serial.println(serial_anfrage);
-     }
-     if (modus == 1){
-       BT.println(serial_anfrage);
-     }
-   }
-   stat = true;
- }
- else{
-   stat = false;
- }
- return stat;
+    else{
+      if (modus == 0){
+        Serial.println(serial_anfrage);
+      }
+      if (modus == 1){
+        BT.println(serial_anfrage);
+      }
+    }
+    stat = true;
+  }
+  else{
+    stat = false;
+  }
+  return stat;
 }
 
 void loop() {
-  
+
+  char status;
+  double T,P;
+
+  status = BMP180.startTemperature();
+
+  while (true){
+    if (status != 0){
+      // Warte bis die Messung komplett ist:
+      delay(status);
+
+      // Extrahiere die Temperatur aus der Messung
+      // Die Messung wird in der Variablen T gespeichert
+      // Funktionen liefern 1 wenn erfolgreich, 0 wenn nicht.
+
+      status = BMP180.getTemperature(T);
+      if (status != 0){
+        status = BMP180.startPressure(3);
+        if (status != 0){
+          delay(status);
+          status = BMP180.getPressure(P,T);
+          if (status != 0){
+            //Messung erfolgreich, geh raus aus der Schleife
+            break;
+          }
+          else{
+            //Warte 250 ms und probiere nochmal
+            delay(250);
+          }
+        }
+      } 
+    }
+  }
+
   //Beinhaltet die aktuellen Messwerte
   String json_string = "";
-  
+
   //Sensoren Variablen
   double feuchte = 0.0;
-  double temperatur = 0.0;
+  double temp_am2302 = 0.0;
   double widerstand = 0.0;
-  
+  double temp_bmp180 = static_cast<double>(T);
+  int druckwert = static_cast<int>(P);
+
   //Serialport Variablen
   String serial_anfrage;
-  
+
   //Lese die Sensoren x mal aus
   int anzahl_messungen = 5;
   for (int i = 0; i < anzahl_messungen; i++){
-    
+
     // Wartezeit zwischen den Messungen.
     delay(500);
 
     feuchte += dht.readHumidity();
-    temperatur += dht.readTemperature();
+    temp_am2302 += dht.readTemperature();
     widerstand += analogRead(0);
-   
+
     // Wenn das Auslesen Fehlschlägt so schreib die Meldung und versuch es nochmal.
-    if (isnan(feuchte) || isnan(temperatur)) {
+    if (isnan(feuchte) || isnan(temp_am2302)) {
       if (SerialMethod == 0){
-          Serial.println(Fehlermeldung);
+        Serial.println(Fehlermeldung);
       }
       if (SerialMethod == 1){
         BT.println(Fehlermeldung);
       }
-        return;
-      }
+      return;
+    }
   }
-  
+
   //Bestimme den Mittelwert
-  feuchte = feuchte/(double)anzahl_messungen;
-  temperatur = temperatur/(double)anzahl_messungen;
-  widerstand = widerstand/(double)anzahl_messungen;
-  
+  feuchte = feuchte/static_cast<double>(anzahl_messungen);
+  temp_am2302 = temp_am2302/static_cast<double>(anzahl_messungen);
+  widerstand = widerstand/static_cast<double>(anzahl_messungen);
+
   //Generiere das Json Objekt
-  json_string = json_generator(feuchte, temperatur, widerstand);
-  
+  json_string = json_generator(feuchte, temp_am2302, temp_bmp180, druckwert, widerstand);
+
   //Lese die serielle Anfrage aus
   serial_anfrage = seriell_auslesen(SerialMethod);
   //Antworte auf die serielle Anfrage
   seriell_senden(SerialMethod, serial_anfrage, json_string);
-  
 }
+
